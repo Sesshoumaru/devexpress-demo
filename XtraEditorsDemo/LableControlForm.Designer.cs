64df7b30d5c7c5678d0eb87f1a90ccf273daf409
Sesshoumaru <laobi.net@live.com>
2019-05-23 19:05:00 +0800

    label control demo

---
+++ XtraEditorsDemo/LableControlForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace XtraEditorsDemo
+{
+    partial class LableControlForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LableControlForm));
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.SuspendLayout();
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.AllowHtmlString = true;
+            this.labelControl1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelControl1.Location = new System.Drawing.Point(21, 12);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(151, 14);
+            this.labelControl1.TabIndex = 0;
+            this.labelControl1.Text = "<b>Click</b> to open our <href=\"http://www.devexpress.com\">Web-site</href>";
+            this.labelControl1.HyperlinkClick += new DevExpress.Utils.HyperlinkClickEventHandler(this.labelControl1_HyperlinkClick);
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Image = ((System.Drawing.Image)(resources.GetObject("labelControl2.Appearance.Image")));
+            this.labelControl2.ImageAlignToText = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            this.labelControl2.Location = new System.Drawing.Point(21, 47);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(91, 20);
+            this.labelControl2.TabIndex = 1;
+            this.labelControl2.Text = "labelControl2";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Image = ((System.Drawing.Image)(resources.GetObject("labelControl3.Appearance.Image")));
+            this.labelControl3.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.labelControl3.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.labelControl3.ImageAlignToText = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            this.labelControl3.LineLocation = DevExpress.XtraEditors.LineLocation.Center;
+            this.labelControl3.LineOrientation = DevExpress.XtraEditors.LabelLineOrientation.Horizontal;
+            this.labelControl3.LineVisible = true;
+            this.labelControl3.Location = new System.Drawing.Point(21, 82);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(219, 20);
+            this.labelControl3.TabIndex = 2;
+            this.labelControl3.Text = "labelControl3";
+            // 
+            // LableControlForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 306);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Name = "LableControlForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+    }
+}
+
---
+++ XtraEditorsDemo/LableControlForm.cs
@@ -0,0 +1,18 @@
+﻿using DevExpress.Utils;
+using DevExpress.XtraEditors;
+
+namespace XtraEditorsDemo
+{
+    public partial class LableControlForm : XtraForm
+    {
+        public LableControlForm()
+        {
+            InitializeComponent();
+        }
+
+        private void labelControl1_HyperlinkClick(object sender, HyperlinkClickEventArgs e)
+        {
+            XtraMessageBox.Show(e.Link);
+        }
+    }
+}
